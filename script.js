77112d64b1ed4fdd7a2814fa8b730544fcb7f86d
akbar <akbarmowahed@yahoo.com>
2024-05-25 20:29:23 +0430

    it commit to git

---
+++ script.js
@@ -0,0 +1,375 @@
+
+addEventListener("keypress",(event)=>{
+    if(event.code=="KeyA"){
+        let button=document.getElementById("a");
+        button.classList.toggle("active")
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("a");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyB"){
+        let button=document.getElementById("b");
+        button.classList.toggle("active")
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("b");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyC"){
+        let button=document.getElementById("c");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("c");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyD"){
+        let button=document.getElementById("d");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("d");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyE"){
+        let button=document.getElementById("e");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("e");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyF"){
+        let button=document.getElementById("f");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("f");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyG"){
+        let button=document.getElementById("g");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("g");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyH"){
+        let button=document.getElementById("h");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("h");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyJ"){
+        let button=document.getElementById("j");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("j");
+        deleteClass.classList.remove("active");
+        })
+    }
+
+    else if(event.code=="KeyI"){
+        let button=document.getElementById("i");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("i");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyK"){
+        let button=document.getElementById("k");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("k");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyL"){
+        let button=document.getElementById("l");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("l");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyM"){
+        let button=document.getElementById("m");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("m");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyN"){
+        let button=document.getElementById("n");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("n");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyO"){
+        let button=document.getElementById("o");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("o");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyP"){
+        let button=document.getElementById("p");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("p");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyQ"){
+        let button=document.getElementById("q");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("q");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyR"){
+        let button=document.getElementById("r");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("r");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyS"){
+        let button=document.getElementById("s");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("s");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyT"){
+        let button=document.getElementById("t");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("t");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyU"){
+        let button=document.getElementById("u");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("u");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyV"){
+        let button=document.getElementById("v");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("v");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyW"){
+        let button=document.getElementById("w");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("w");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyX"){
+        let button=document.getElementById("x");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("x");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyY"){
+        let button=document.getElementById("y");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("y");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="KeyZ"){
+        let button=document.getElementById("z");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("z");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit1"){
+        let button=document.getElementById("one");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("one");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit2"){
+        let button=document.getElementById("two");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("two");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit3"){
+        let button=document.getElementById("three");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("three");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit4"){
+        let button=document.getElementById("four");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("four");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit5"){
+        let button=document.getElementById("five");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("five");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit6"){
+        let button=document.getElementById("six");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("six");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit7"){
+        let button=document.getElementById("seven");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("seven");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit8"){
+        let button=document.getElementById("eight");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("eight");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit9"){
+        let button=document.getElementById("nine");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("nine");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Digit0"){
+        let button=document.getElementById("zero");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("zero");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Equal"){
+        let button=document.getElementById("inc");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("inc");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Minus"){
+        let button=document.getElementById("dec");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("dec");
+        deleteClass.classList.remove("active");
+        })
+    }
+    
+    else if(event.code=="BracketLeft"){
+        let button=document.getElementById("bk1");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("bk1");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="BracketRight"){
+        let button=document.getElementById("bk2");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("bk2");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Semicolon"){
+        let button=document.getElementById("sm");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("sm");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Quote"){
+        let button=document.getElementById("qt");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("qt");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Comma"){
+        let button=document.getElementById("coma");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("coma");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Period"){
+        let button=document.getElementById("dote");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("dote");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Slash"){
+        let button=document.getElementById("question");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("question");
+        deleteClass.classList.remove("active");
+        })
+    }
+    else if(event.code=="Space"){
+        let button=document.getElementById("space");
+        button.classList.toggle("active");
+        addEventListener('keyup',(event)=>{
+        let deleteClass=document.getElementById("space");
+        deleteClass.classList.remove("active");
+        })
+    }
+})
+
+
